3961831324e7ddd0ea4ffcac97d32e2fdd77a3c3
Sahil Narula <126839546+Hunkhill1@users.noreply.github.com>
2024-05-06 22:01:33 +0800

    Have python server running and listeing on multiple ports; have the ability to kill ports in use; is hard coded at the moment to get data; need to fix

--- station-server.py
+++ station-server.py
@@ -1,4 +1,5 @@
-#Needs to start server from this .py#Invokes this way: ./station-server.py [station name] [TCP port] [UDP port] [neighbour(s)]
+#Needs to start server from this .py
+#Invokes this way: ./station-server.py [station name] [TCP port] [UDP port] [neighbour(s)]
 #accept query from tcp port and bidirectional from UDP port 
 #call timetable.py to get fastest times to reach my neighbours --> in the form of --> 
 #---> (APPEND DESTINATION HERE BECAUSE WE GET THIS FROM HTML) Departure time, route name, departing from, arrival time, arrival station 
