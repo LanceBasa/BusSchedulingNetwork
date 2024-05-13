49ab52aacbe5867b9955486a76942f3b0c0d8f9b
MidhinV <midhinvarkeykaraparambil@gmail.com>
2024-05-13 20:04:08 +0800

    protocol.c TCP&UDP works, T&U.c is the same (backup)

--- Getting Started/T&U.c
+++ Getting Started/T&U.c
@@ -176,12 +176,3 @@ int main(int argc, char* argv[]) {
     close(udp_sock);
     return 0;
 }
-
-
-//./protocol BusportB 4003 4004 localhost:4006 localhost:4010 
-//./protocol StationC 4005 4006 localhost:4004 localhost:4008 localhost:4010 
-// ./protocol JunctionE 4009 4010 localhost:4002 localhost:4004 localhost:4006 
-
-//./protocol JunctionA 4001 4002 localhost:4010 localhost:4012 
-//./protocol TerminalD 4007 4008 localhost:4006 
-//./protocol BusportF 4011 4012 localhost:4002 
